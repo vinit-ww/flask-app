9d9ffb01170f0be4c92b8c0b865f2ae5c2fbfeb0
vinit-ww <vineet.kothari@wwindia.com>
2017-03-21 20:04:20 +0530

    Update config.py

--- demo-app-v1/config.py
+++ demo-app-v1/config.py
@@ -2,5 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:default@127.0.0.1:3306/mysql'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@server/db'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')

00c5ee313a1734d9a07debaf0450fe5a385eaa71
vinit-ww <vineet.kothari@wwindia.com>
2017-03-21 20:05:01 +0530

    Update models.py

--- demo-app-v1/app/models.py
+++ demo-app-v1/app/models.py
@@ -1,6 +1,6 @@
 from app import db
 
-class Table3(db.Model):
+class Table(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
